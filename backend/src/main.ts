78d4852fcebc7a9b7e27774bec590184778e96b3
grecha1337 <4rmor3d@gmail.com>
2023-02-06 23:30:50 +0300

    change cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: [
+      'http://localhost:3000',
+      'https://mysuperfront.students.nomoredomains.icu',
+    ],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3000);
 }
