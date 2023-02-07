c7d079e836e191129b2d5887f2bdbe1b8d921e5c
grecha1337 <4rmor3d@gmail.com>
2023-02-07 13:23:10 +0300

    change cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors();
   await app.listen(3000);
 }
 console.log('POSTGRES_USER', process.env.POSTGRES_USER);
